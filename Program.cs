eaeadebf7278e58a3b85058c4f332d695f354762
drfrostongithub <eternal.putra@gmail.com>
2020-11-17 21:06:53 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sir!");
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date}!");
+            Console.Write("\nPress any key to exit ... ");
+            Console.ReadKey(false);
+        }
+    }
+}
